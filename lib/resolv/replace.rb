b9aae0005d444633536377b8d0bde19fa4d967f9
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2020-04-03 09:48:36 +0900

    bundle gem resolve-replace

---
+++ lib/resolv/replace.rb
@@ -0,0 +1,8 @@
+require "resolv/replace/version"
+
+module Resolv
+  module Replace
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/resolv/replace/version.rb
@@ -0,0 +1,5 @@
+module Resolv
+  module Replace
+    VERSION = "0.1.0"
+  end
+end
